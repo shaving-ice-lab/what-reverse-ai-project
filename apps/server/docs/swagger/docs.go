2a1a3fcf63f3ce7f7b1886fb7c746a6da53005a6
Vantiboolean <lhz1208@gmail.com>
2026-02-12 17:24:52 +0800

    docs(swagger): 更新Swagger文档中的服务端口为3010

--- apps/server/docs/swagger/docs.go
+++ apps/server/docs/swagger/docs.go
@@ -35,7 +35,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "localhost:8080",
+	Host:             "localhost:3010",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "reverseai API",
